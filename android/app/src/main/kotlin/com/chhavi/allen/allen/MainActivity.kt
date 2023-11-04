f7512f7624a3ed18735713ecef9ec993b22a300e
Chhavi <127318630+Kgotta-contribute@users.noreply.github.com>
2023-11-04 07:56:46 +0530

    Updates

---
+++ android/app/src/main/kotlin/com/chhavi/allen/allen/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.chhavi.allen.allen
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/secrets.dart
@@ -0,0 +1,2 @@
+//const openAIAPIKey = "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";
+const openAIAPIKey1 = "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";
